86dffa719b1bb4b59deed4bb80688358afbcc30d
thegreatestacc <67763738+thegreatestacc@users.noreply.github.com>
2020-08-10 18:41:19 +0300

    commit

--- src/main/java/net/sovliv/jsontestapp/controllers/UserController.java
+++ src/main/java/net/sovliv/jsontestapp/controllers/UserController.java
@@ -2,48 +2,74 @@ package net.sovliv.jsontestapp.controllers;
 
 import net.sovliv.jsontestapp.domain.User;
 import net.sovliv.jsontestapp.service.UserService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Optional;
-
+/**
+ * User controller
+ */
 @RestController
 @RequestMapping("/users")
 public class UserController {
-    Logger logger = LoggerFactory.getLogger(UserController.class);
     private UserService userService;
 
     public UserController(UserService userService) {
         this.userService = userService;
     }
 
+    /**
+     * Просмотр всей информации о всех пользователях
+     * @return
+     */
     @GetMapping("/list")
     public Iterable<User> list() {
         return userService.list();
     }
 
+    /**
+     * Просмотр инофрмации об одном пользователе
+     * @param id айди по которому будет
+     * @return
+     */
     @GetMapping("/list/{id}")
-    public Optional<User> getOneUser(@PathVariable("id") Long id, String name) {
-        for (User user : list()) {
-            if(user.getId().equals(id)) {
-                name = user.getName();
-            }
-        }
-        logger.info("you get info about user with name " + name);
+    public User getOneUser(@PathVariable("id") Long id) {
         return userService.getOneUser(id);
     }
 
-    @DeleteMapping("/list/delete/{id}")
-    public void deleteUser(@PathVariable("id") User user) {
-        userService.deleteUser(user);
-        logger.info("user deleted");
-
+    /**
+     * Добавление нового пользователя
+     * @param newUser новый пользователь
+     * @return
+     */
+    @PostMapping("/list/add")
+    public User addUser(@RequestBody User newUser) {
+        return userService.create(newUser);
     }
 
-    @DeleteMapping("/list/delete")
+    /**
+     * Редактирование информации одного пользователя
+     * @param newUser новый пользователь
+     * @param id айди редактируемого пользователя
+     * @return
+     */
+    @PutMapping("/list/edit/{id}")
+    public User editUser(@RequestBody User newUser, @PathVariable Long id) {
+        return userService.update(newUser, id);
+    }
+
+    /**
+     * Удаление одного пользователя
+     * @param id айди по которому будет удален пользователь
+     */
+    @DeleteMapping("/list/delete/{id}")
+    public void deleteUser(@PathVariable("id") Long id) {
+        userService.deleteUserById(id);
+    }
+
+    /**
+     * Удаление всех пользователей
+     */
+    @DeleteMapping("/list/deleteAll")
     public void deleteAllUsers() {
         userService.deleteAllUsers();
-        logger.info("all users deleted");
     }
 }
--- src/main/java/net/sovliv/jsontestapp/service/UserService.java
+++ src/main/java/net/sovliv/jsontestapp/service/UserService.java
@@ -2,6 +2,8 @@ package net.sovliv.jsontestapp.service;
 
 import net.sovliv.jsontestapp.domain.User;
 import net.sovliv.jsontestapp.repository.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -9,6 +11,7 @@ import java.util.Optional;
 
 @Service
 public class UserService {
+    Logger logger = LoggerFactory.getLogger(UserService.class);
 
     private UserRepository userRepository;
 
@@ -17,26 +20,46 @@ public class UserService {
     }
 
     public Iterable<User> list() {
+        logger.info("all users found");
         return userRepository.findAll();
     }
 
-    public Optional<User> getOneUser(Long id) {
-        return userRepository.findById(id);
+    public User getOneUser(Long id) {
+        Optional<User> optional = userRepository.findById(id);
+        if (!optional.isPresent()) {
+            throw new RuntimeException("user not found");
+        }
+        logger.info("info about one user");
+        return optional.get();
     }
 
-    public User save(User user) {
+    public User create(User user) {
+        logger.info("user created");
+        return userRepository.save(user);
+    }
+    
+    public User update(User user, Long id) {
+        Optional<User> optional = userRepository.findById(id);
+        if (!optional.isPresent()) {
+            throw new RuntimeException("user not found");
+        }
+        user.setId(id);
+        logger.info("user updated");
         return userRepository.save(user);
     }
 
     public Iterable<User> saveAll(List<User> users) {
+        logger.info("all users saved");
         return userRepository.saveAll(users);
     }
 
-    public void deleteUser(User user) {
-        userRepository.delete(user);
+    public void deleteUserById(Long id) {
+        logger.info("user deleted");
+        userRepository.deleteById(id);
     }
 
     public void deleteAllUsers() {
+        logger.info("all users deleted");
         userRepository.deleteAll();
     }
 }
